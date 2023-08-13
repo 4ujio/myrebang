cc7c126b227f473d554e4e8535cfcc8071126b34
朴树 <2155685509@qq.com>
2023-08-13 18:41:01 +0800

    13

---
+++ src/components/ConRight.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="con-right" style="color: white">
+    <div class="right_hea">
+      <img :src="tabcon.img" alt="" />
+      <span>{{ tabcon.title }}</span>
+      <svg
+        data-v-d7b5176e=""
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1024 1024"
+      >
+        <path
+          fill="currentColor"
+          d="M512 64a448 448 0 1 1 0 896.064A448 448 0 0 1 512 64zm67.2 275.072c33.28 0 60.288-23.104 60.288-57.344s-27.072-57.344-60.288-57.344c-33.28 0-60.16 23.104-60.16 57.344s26.88 57.344 60.16 57.344zM590.912 699.2c0-6.848 2.368-24.64 1.024-34.752l-52.608 60.544c-10.88 11.456-24.512 19.392-30.912 17.28a12.992 12.992 0 0 1-8.256-14.72l87.68-276.992c7.168-35.136-12.544-67.2-54.336-71.296-44.096 0-108.992 44.736-148.48 101.504 0 6.784-1.28 23.68.064 33.792l52.544-60.608c10.88-11.328 23.552-19.328 29.952-17.152a12.8 12.8 0 0 1 7.808 16.128L388.48 728.576c-10.048 32.256 8.96 63.872 55.04 71.04 67.84 0 107.904-43.648 147.456-100.416z"
+        ></path>
+      </svg>
+    </div>
+    <ul>
+      <li v-for="(item, index) in tabcon.data" :key="index">
+        <b v-color="index">{{ ++index }}</b>
+        <span>{{ item.title }}</span>
+        <i style="font-style: normal">{{ item.hotValue }}</i>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    color(i, n) {
+      if (n.value == 0) {
+        i.style.background = "#fe2d46";
+      }
+      if (n.value == 1) {
+        i.style.background = "#ff6600";
+      }
+      if (n.value == 2) {
+        i.style.background = "#faa90e";
+      }
+    },
+  },
+  props: {
+    tabcon: {
+      type: [Object, Array],
+      required: true,
+    },
+  },
+  data() {
+    return {
+      dataobj: {},
+      num: 0,
+    };
+  },
+  //   watch: {
+  //     tabcon(newValue) {
+  //       this.arr = newValue;
+  //     },
+  //   },
+  mounted() {
+    this.dataobj = this.tabcon;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.con-right {
+  width: 73%;
+  height: 100%;
+  padding: 0 2%;
+  //   background: red;
+  .right_hea {
+    width: 100%;
+    height: 10%;
+    border-bottom: 1px solid rgba(245, 245, 245, 0.04);
+    display: flex;
+    align-items: center;
+    position: relative;
+    img {
+      width: 20px;
+      height: 20px;
+      margin-right: 10px;
+      display: block;
+      border-radius: 50%;
+    }
+    span {
+      color: #dfdddd;
+      font-size: 20px;
+    }
+    svg {
+      position: absolute;
+      right: 0;
+      width: 17px;
+      height: 17px;
+    }
+  }
+  ul {
+    overflow: auto;
+    scrollbar-width: thin; /* Firefox */
+    scrollbar-color: transparent transparent; /* Firefox */
+  }
+
+  /* 定制滚动条样式 */
+  ul::-webkit-scrollbar {
+    width: 4px;
+    height: 30px;
+  }
+
+  ul::-webkit-scrollbar-thumb {
+    background-color: gray; /* 滚动条颜色 */
+  }
+
+  ul::-webkit-scrollbar-track {
+    background-color: transparent; /* 轨道背景颜色 */
+  }
+  ul {
+    width: 100%;
+    height: 90%;
+    // overflow: auto;
+    position: relative;
+
+    li {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      padding: 8px 0;
+      color: #dfdddd;
+      b {
+        display: block;
+        width: 20px;
+        height: 20px;
+        background: #25252c;
+        font-weight: normal;
+        line-height: 18px;
+        text-align: center;
+        border-radius: 4px;
+        margin-right: 15px;
+        font-size: 12px;
+      }
+      i {
+        position: absolute;
+        right: 0;
+        color: gray;
+        font-size: 14px;
+      }
+    }
+    li:hover {
+      color: #1981ff;
+    }
+  }
+}
+</style>
--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -62,25 +62,239 @@
       </b>
     </span>
     <img class="title_img" src="../assets/热搜榜.png" alt="" />
-    <Header />
+    <div id="box">
+      <div class="box_con">
+        <p>
+          <svg
+            t="1691745490410"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="12435"
+            width="16"
+            height="16"
+          >
+            <path
+              d="M729.6 931.2l-416-425.6 416-416c9.6-9.6 9.6-25.6 0-35.2-9.6-9.6-25.6-9.6-35.2 0l-432 435.2c-9.6 9.6-9.6 25.6 0 35.2l432 441.6c9.6 9.6 25.6 9.6 35.2 0C739.2 956.8 739.2 940.8 729.6 931.2z"
+              p-id="12436"
+              fill="#e6e6e6"
+            ></path>
+          </svg>
+        </p>
+        <ul>
+          <li
+            :tabdata="arr"
+            @click="lifun(index)"
+            v-for="(item, index) in tab"
+            :key="index"
+            :class="item.flag ? 'li_true' : ''"
+          >
+            {{ item.title }}
+          </li>
+        </ul>
+        <p>
+          <svg
+            t="1691745505953"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="12638"
+            width="16"
+            height="16"
+          >
+            <path
+              d="M761.6 489.6l-432-435.2c-9.6-9.6-25.6-9.6-35.2 0-9.6 9.6-9.6 25.6 0 35.2l416 416-416 425.6c-9.6 9.6-9.6 25.6 0 35.2s25.6 9.6 35.2 0l432-441.6C771.2 515.2 771.2 499.2 761.6 489.6z"
+              p-id="12639"
+              fill="#e6e6e6"
+            ></path>
+          </svg>
+        </p>
+      </div>
+    </div>
+    <div class="con">
+      <ConLeft :tabdata="arr" @ss="sssFun" :flag="flag" />
+      <ConRight :tabcon="arr[a]" />
+    </div>
   </div>
 </template>
 
 <script>
-import Header from "../components/HeaderView.vue";
+//数据
+import conarr from "../components/arr.json";
+//身体左侧导航
+import ConLeft from "../components/ConLeft.vue";
+//身体主体
+import ConRight from "../components/ConRight.vue";
 export default {
   data() {
     return {
       bigflag: false,
+      arr: [],
+      tab: [
+        {
+          title: "我的订阅",
+          flag: false,
+        },
+        {
+          title: "全部",
+          flag: true,
+        },
+        {
+          title: "综合",
+          flag: false,
+        },
+        {
+          title: "科技",
+          flag: false,
+        },
+        {
+          title: "娱乐",
+          flag: false,
+        },
+        {
+          title: "社区",
+          flag: false,
+        },
+        {
+          title: "购物",
+          flag: false,
+        },
+        {
+          title: "财经",
+          flag: false,
+        },
+        {
+          title: "大学",
+          flag: false,
+        },
+      ],
+      a: 0,
+      //我的订阅状态
+      flag: false,
     };
   },
   components: {
-    Header,
+    ConLeft,
+    ConRight,
+  },
+  methods: {
+    sssFun(n) {
+      console.log(n);
+      this.a = n;
+    },
+    lifun(sindex) {
+      this.a = 0;
+      //传递数据
+      this.arr = [];
+      //我的订阅
+      if (sindex == 0) {
+        this.arr = conarr.filter((item) => item.dingyue == "true");
+        this.flag = true;
+      }
+      //全部
+      if (sindex == 1) {
+        this.arr = conarr;
+        this.flag = false;
+      }
+      if (sindex > 1) {
+        this.flag = false;
+        conarr.forEach((item) => {
+          if (item.from == JSON.stringify(sindex - 1)) {
+            this.arr.push(item);
+          }
+        });
+      }
+      // console.log(this.arr);
+
+      // this.$refs.childComponent.updateData(this.dataFromParent);
+      //li选中状态
+      this.tab.forEach((item, index) => {
+        if (sindex == index) {
+          item.flag = true;
+        } else {
+          item.flag = false;
+        }
+      });
+    },
+  },
+  mounted() {
+    conarr.forEach((item) => {
+      this.arr.push(item);
+    });
   },
 };
 </script>
 
 <style lang="scss" scoped>
+//身体
+.con {
+  width: 100%;
+  height: 84%;
+  display: flex;
+  flex-direction: row;
+  margin-top: 1.5%;
+  border-top: 1px solid rgba(245, 245, 245, 0.04);
+}
+//头部
+#box {
+  width: 96%;
+  height: 32px;
+  margin: auto;
+  padding-top: 50px;
+  position: relative;
+  z-index: -1;
+  border-bottom: 3px solid #414243;
+  color: white;
+  .box_con {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    ul {
+      position: absolute;
+      width: 94%;
+      height: 35px;
+      list-style: none;
+      display: flex;
+      justify-content: space-between;
+      left: 3%;
+      bottom: -3px;
+      font-size: 14px;
+      li {
+        padding: 0 3px;
+        letter-spacing: 1px;
+        transition: all 0.1s linear;
+      }
+      li:hover {
+        color: #1772de !important;
+      }
+      .li_true {
+        // padding: 0 10px;
+        border-bottom: 3px solid #1681ff !important;
+        color: #1772de !important;
+      }
+      .li_false {
+        border-bottom: 3px solid rgba(0, 0, 0, 0);
+      }
+    }
+    p {
+      display: block;
+      display: flex;
+      justify-content: center;
+      width: 3%;
+      height: 32px;
+      background: #1c1c1e;
+      svg {
+        width: 14px;
+        height: 14px;
+        margin-top: 2px;
+      }
+    }
+  }
+}
 #xiao {
   width: 1000px !important;
   height: 650px !important;
